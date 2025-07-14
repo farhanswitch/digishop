68070a3aa15c23ce623442872d73ab839c5a30d7
farhanswitch <muhammad_farhan@outlook.co.id>
2025-07-15 05:39:07 +0700

    Adjust register controller. Add password validation and decrypt encrypted password

--- modules/users/user.controller.go
+++ modules/users/user.controller.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"unicode"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -16,6 +17,24 @@ type userController struct {
 
 var controller userController
 
+func passwordValidation(fl validator.FieldLevel) bool {
+	password := fl.Field().String()
+
+	var hasUpper, hasNumber, hasSpecial bool
+
+	for _, ch := range password {
+		switch {
+		case unicode.IsUpper(ch):
+			hasUpper = true
+		case unicode.IsNumber(ch):
+			hasNumber = true
+		case unicode.IsPunct(ch) || unicode.IsSymbol(ch):
+			hasSpecial = true
+		}
+	}
+
+	return hasUpper && hasNumber && hasSpecial
+}
 func (u userController) RegisterUserCtrl(w http.ResponseWriter, r *http.Request) {
 	var param RegisterUserRequest
 	w.Header().Set("Content-Type", "application/json")
@@ -26,6 +45,7 @@ func (u userController) RegisterUserCtrl(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	val := validator.New()
+	val.RegisterValidation("password", passwordValidation)
 	err = val.Struct(param)
 	if err != nil {
 		log.Println(err)
--- modules/users/user.dto.go
+++ modules/users/user.dto.go
@@ -8,10 +8,12 @@ type RegisterUserRequest struct {
 	Username    string `json:"username" validate:"required,min=6,max=15,alphanum"`
 	FirstName   string `json:"firstName" validate:"required,min=3,max=50"`
 	LastName    string `json:"lastName" validate:"min=0,max=50"`
-	Password    string `json:"password" validate:"required"`
-	Email       string `json:"email" validate:"required,email"`
-	PhoneNumber string `json:"phoneNumber" validate:"required,min=10,max=15"`
-	ID          string `json:"id"`
+	Password    string `json:"password" validate:"required,password"`
+
+	ConfirmPassword string `json:"confirmPassword" validate:"required"`
+	Email           string `json:"email" validate:"required,email"`
+	PhoneNumber     string `json:"phoneNumber" validate:"required,min=10,max=15"`
+	ID              string `json:"id"`
 }
 
 type LoginUserRequest struct {
