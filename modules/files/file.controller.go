1de12112b08f11a7d2d5a19f9c653c55a5e3f02b
Muhammad Farhan <muhammad_farhan@outlook.co.id>
2025-07-24 11:17:57 +0700

    Handle max size and valid mime type

--- modules/files/file.controller.go
+++ modules/files/file.controller.go
@@ -6,6 +6,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"slices"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type fileController struct {
@@ -14,8 +19,9 @@ type fileController struct {
 var controller fileController
 
 func (f fileController) UploadFileCtrl(w http.ResponseWriter, r *http.Request) {
+	validMaxSize := 8 << 20
 	w.Header().Set("Content-Type", "application/json")
-	r.ParseMultipartForm(10 << 20)
+	r.ParseMultipartForm(8 << 20)
 	file, handler, err := r.FormFile("file")
 	if err != nil {
 		log.Println("Error Retrieving the File")
@@ -25,10 +31,49 @@ func (f fileController) UploadFileCtrl(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer file.Close()
+	if handler.Size > int64(validMaxSize) {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, `{"errors":"File size is too large"}`)
+		return
+	}
 	log.Printf("Uploaded File: %+v\n", handler.Filename)
 	log.Printf("File Size: %+v\n", handler.Size)
 	log.Printf("MIME Header: %+v\n", handler.Header)
-	tempFile, err := os.CreateTemp("uploads", "upload-*.png")
+	var listValidMimeType []string = []string{"image/png", "image/jpeg", "image/jpg", "image/webp", "application/octet-stream"}
+	fileContentType := handler.Header.Get("Content-Type")
+	var isValidMimeType bool = false
+	if slices.Contains(listValidMimeType, string(fileContentType)) {
+		isValidMimeType = true
+	}
+	if !isValidMimeType {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, `{"errors":"Invalid file type"}`)
+		return
+	}
+	var fileExtension string = filepath.Ext(handler.Filename)
+	if fileExtension == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, `{"errors":"Invalid file type"}`)
+		return
+	}
+	strUUID, err := uuid.NewV7()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, `{"error":"%s"}`, err.Error())
+		return
+	}
+	var newFileName string = strUUID.String() + fileExtension
+	var fileNamePatternToSave string = "uploads/" + fmt.Sprintf("%d", time.Now().Unix()) + "--" + strUUID.String() + fileExtension
+	var dataToSave map[string]interface{} = map[string]interface{}{
+		"file_name":          newFileName,
+		"file_extension":     fileExtension,
+		"original_file_name": handler.Filename,
+		"content_type":       fileContentType,
+	}
+	log.Println(dataToSave, time.Now().Unix())
+
+	tempFile, err := os.Create(fileNamePatternToSave)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
